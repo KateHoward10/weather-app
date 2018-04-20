83f51ed5a63af6e87d144457dc359df4a1be39c4
Kate Howard <kate@kate.com>
2018-04-20 20:43:48 +0100

    Investigating Highcharts, meanwhile app is printing out temp data for next 5 days

--- weather-forecaster/src/components/Info.js
+++ weather-forecaster/src/components/Info.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import moment from 'moment';
+// var Highcharts = require('highcharts');
+
+// // Load module after Highcharts is loaded
+// require('highcharts/modules/exporting')(Highcharts);
 
 class Info extends Component {
   state = {
     id: "",
     weather: [],
+    today: [],
     sunset: "",
   }
 
@@ -17,10 +22,12 @@ class Info extends Component {
         this.setState({ id: id });
         axios.get(`http://interview.toumetisanalytics.com/weather/${id}`)
 	      .then(response => {
-	      	const weather = response.data.consolidated_weather[0];
+	      	const weather = response.data.consolidated_weather;
+	      	const today = response.data.consolidated_weather[0];
 	      	const sunset = response.data.sun_set;
 	      	this.setState({
 	      		weather: weather,
+	      		today: today,
 	      		sunset: sunset
 	      	});
 	    })
@@ -28,13 +35,26 @@ class Info extends Component {
   }
 
   render() {
-  	const {weather, sunset} = this.state;
+  	const {weather, today, sunset} = this.state;
   	const {location} = this.props;
+  	console.log(weather);
     return (
-      	<div>
-			<p>Current temperature in {location}: {weather.the_temp}</p>
-			<p>Current weather: {weather.weather_state_name}</p>
-			<p>Sundown: {moment(sunset).fromNow()}</p>
+    	<div>
+	    	<h3>{location.toUpperCase()}</h3>
+      		<div className="container">
+      			<div className="row">
+					<div className="col-4"><p><b>Current temperature</b></p><p>{Math.round(today.the_temp)}<sup>o</sup>C</p></div>
+					<div className="col-4"><p><b>Current weather</b></p><p>{today.weather_state_name}</p></div>
+					<div className="col-4"><p><b>Sundown</b></p><p>{moment(sunset).fromNow()}</p></div>
+				</div>
+				{weather.map((day, index) => 
+				<p key={index}>{moment(day.applicable_date).format('dddd')}: min {Math.round(day.min_temp)}<sup>o</sup>C, max {Math.round(day.max_temp)}<sup>o</sup>C, ave {Math.round(day.the_temp)}<sup>o</sup>C</p>
+				)}
+			</div>
+				
+			<div id="container" className="chart">
+				
+			</div>
 		</div>
     )
   }
