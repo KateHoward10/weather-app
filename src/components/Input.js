6266c76fa7c3270af1f19088ab995a6ff113e523
Kate Howard <k.l.howard.1992@gmail.com>
2019-07-16 23:27:07 +0100

    Find current location

--- src/components/Input.js
+++ src/components/Input.js
@@ -11,12 +11,33 @@ function Input(props) {
 		chart(search);
 	}
 
+	function getLocation(e) {
+		e.preventDefault();
+		const geolocation = navigator.geolocation;
+
+		if (geolocation) {
+			geolocation.getCurrentPosition(position => showPosition(position), error => alert(error.message), {
+				enableHighAccuracy: true,
+				timeout: 20000,
+				maximumAge: 1000
+			});
+		}
+	}
+
+	function showPosition(position) {
+		const coords = `${position.coords.latitude}, ${position.coords.longitude}`;
+		setSearch(coords);
+	}
+
 	return (
 		<form>
-			<input type="text" onChange={e => setSearch(e.target.value)} />
+			<input type="text" value={search} onChange={e => setSearch(e.target.value)} />
 			<button type="submit" onClick={onSubmit}>
 				Submit
 			</button>
+			<p>
+				or, <button onClick={getLocation}>get current location</button>
+			</p>
 		</form>
 	);
 }
