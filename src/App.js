c51d3104c50ad914a9c2e386342853a5e25486ba
Kate Howard <k.l.howard.1992@gmail.com>
2019-07-17 19:43:47 +0100

    Background colour corresponds with time of day

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Weather from './components/Weather';
+import { submit } from './data/actions';
+import { getInfo, getData } from './data/api';
 import './App.css';
-import WeatherContainer from './WeatherContainer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Weather Forecaster</h1>
-        </header>
-        <p>To begin, search for a location.</p>
-        <WeatherContainer />
-      </div>
-    );
-  }
-}
+const mapStateToProps = state => {
+	return {
+		location: state.location,
+		weather: state.weather,
+		forecast: state.forecast
+	};
+};
 
-export default App;
+const mapDispatchToProps = dispatch => {
+	return {
+		submit: search => dispatch(submit(search)),
+		display: location => dispatch(getInfo(location)),
+		chart: location => dispatch(getData(location))
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Weather);
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,12 +2,24 @@ import React from 'react';
 import Input from './Input';
 import Info from './Info';
 import Chart from './Chart';
+import { getTime, isNightTime } from '../helpers/date';
 
 const Weather = ({ location, weather, forecast, submit, display, chart }) => (
-	<div>
-		<Input location={location} submit={submit} display={display} chart={chart} />
-		{weather.weather && <Info weather={weather} />}
-		{forecast.list && <Chart location={location} forecast={forecast} />}
+	<div
+		className="App"
+		style={{
+			backgroundColor: weather.sys && isNightTime(weather.sys.sunset) ? 'darkblue' : 'lightblue'
+		}}
+	>
+		<header className="App-header">
+			<h1 className="App-title">Weather Forecaster</h1>
+		</header>
+		<p>To begin, search for a location.</p>
+		<div>
+			<Input location={location} submit={submit} display={display} chart={chart} />
+			{weather.weather && <Info weather={weather} />}
+			{forecast.list && <Chart location={location} forecast={forecast} />}
+		</div>
 	</div>
 );
 
