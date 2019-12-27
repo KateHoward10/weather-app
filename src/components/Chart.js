c647ffc80b66f7403eff3623af8866f1d5c150ae
Kate Howard <k.l.howard.1992@gmail.com>
2019-12-27 19:19:47 +0000

    Use React wrapper for Highcharts

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,74 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import { toCelsius, toColour } from '../helpers/temperature';
 import { getDateRange } from '../helpers/date';
 
-class Chart extends Component {
-	state = {
-		day: 1
-	};
+function Chart({ forecast }) {
+	const [day, setDay] = useState(1);
+	const dataSet = forecast.list.filter((item, index) => index > (day - 1) * 8 - 1 && index < day * 8);
+	const times = dataSet.map(item => moment(item.dt_txt).format('HH:mm'));
 
-	componentDidMount() {
-		this.chart = new Highcharts['Chart'](this.chartEl, this.getOptions());
-	}
-
-	getOptions = () => {
-		const { forecast } = this.props;
-		const { day } = this.state;
-		const dataSet = forecast.list.filter((item, index) => index > (day - 1) * 8 - 1 && index < day * 8);
-		const options = {
+	const options = {
+		title: {
+			text: getDateRange(dataSet)
+		},
+		xAxis: {
+			categories: times
+		},
+		yAxis: {
 			title: {
-				text: getDateRange(dataSet)
-			},
-			xAxis: {
-				categories: dataSet.map(item => moment(item.dt_txt).format('HH:mm'))
-			},
-			yAxis: {
-				title: {
-					text: '°C'
-				}
-			},
-			colors: dataSet.map(item => toColour(item.main.temp)),
-			plotOptions: {
-				column: {
-					colorByPoint: true
-				}
-			},
-			chart: {
-				type: 'column'
-			},
-			legend: {
-				enabled: false
-			},
-			series: [
-				{
-					name: 'Average Temperature',
-					data: dataSet.map(item => toCelsius(item.main.temp))
-				}
-			]
-		};
-		return options;
+				text: '°C'
+			}
+		},
+		colors: dataSet.map(item => toColour(item.main.temp)),
+		plotOptions: {
+			column: {
+				colorByPoint: true
+			}
+		},
+		chart: {
+			type: 'column'
+		},
+		legend: {
+			enabled: false
+		},
+		series: [
+			{
+				name: 'Average Temperature',
+				data: dataSet.map(item => toCelsius(item.main.temp))
+			}
+		]
 	};
 
-	componentDidUpdate() {
-		this.chart.update(this.getOptions());
-	}
-
-	componentWillUnmount() {
-		this.chart.destroy();
-	}
-
-	render() {
-		const { day } = this.state;
-		return (
-			<React.Fragment>
-				<div ref={el => (this.chartEl = el)} />
-				{day > 1 && <button onClick={() => this.setState({ day: day - 1 })}>{`<`}</button>}
-				{day < 5 && <button onClick={() => this.setState({ day: day + 1 })}>></button>}
-			</React.Fragment>
-		);
-	}
+	return (
+		<React.Fragment>
+			<HighchartsReact highcharts={Highcharts} options={options} />
+			{day > 1 && <button onClick={() => setDay(day - 1)}>{`<`}</button>}
+			{day < 5 && <button onClick={() => setDay(day + 1)}>></button>}
+		</React.Fragment>
+	);
 }
 
 export default Chart;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,18 +3,21 @@ import Input from './Input';
 import Info from './Info';
 import Chart from './Chart';
 
-const Weather = ({ location, weather, forecast, submit, display, chart }) => (
-	<div className="App">
-		<header className="App-header">
-			<h1 className="App-title">Weather Forecaster</h1>
-		</header>
-		<p>To begin, search for a location.</p>
-		<div>
-			<Input location={location} submit={submit} display={display} chart={chart} />
-			{weather.weather && <Info weather={weather} />}
-			{forecast.list && <Chart location={location} forecast={forecast} />}
+const Weather = ({ location, weather, forecast, submit, display, chart }) => {
+	console.log(forecast);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-title">Weather Forecaster</h1>
+			</header>
+			<p>To begin, search for a location.</p>
+			<div>
+				<Input location={location} submit={submit} display={display} chart={chart} />
+				{weather.weather && <Info weather={weather} />}
+				{forecast.list && <Chart location={location} forecast={forecast} />}
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Weather;
