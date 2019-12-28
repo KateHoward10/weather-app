373e00bcdc146661f4fc4f432cf42a9f13987320
Kate Howard <k.l.howard.1992@gmail.com>
2019-12-28 20:21:37 +0000

    Add relative time to sunrise/set

--- src/components/SunTracker.js
+++ src/components/SunTracker.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { getTime } from '../helpers/date';
+import { getTime, getRelativeTime } from '../helpers/date';
 import { WiSunrise, WiSunset } from 'react-icons/wi';
 
 const SunTracker = ({ sunrise, sunset }) => (
   <div className="sun-details">
     <div className="sun-time">
       <WiSunrise size="30" color="gold" /> {getTime(sunrise)}
+      <p>{getRelativeTime(sunrise)}</p>
     </div>
     <div className="sun-time">
       <WiSunset size="30" color="orange" /> {getTime(sunset)}
+      <p>{getRelativeTime(sunset)}</p>
     </div>
   </div>
 );
--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,24 +1,29 @@
-import moment from "moment";
+import moment from 'moment';
 
 export const getDateRange = dataSet => {
-	const firstDay = moment(dataSet[0].dt_txt).format("Do MMM");
-	const lastDay = moment(dataSet[dataSet.length-1].dt_txt).format("Do MMM");
-	return `${firstDay}${firstDay!==lastDay ? ` - ${lastDay}` : ``}`;
-}
+	const firstDay = moment(dataSet[0].dt_txt).format('Do MMM');
+	const lastDay = moment(dataSet[dataSet.length - 1].dt_txt).format('Do MMM');
+	return `${firstDay}${firstDay !== lastDay ? ` - ${lastDay}` : ``}`;
+};
 
 export const getTime = time => {
 	const timeInSeconds = new Date(time * 1000);
-	return moment(timeInSeconds).format("HH:mm");
-}
+	return moment(timeInSeconds).format('HH:mm');
+};
 
-export const isNightTime = (sunset) => {
+export const isNightTime = sunset => {
 	const set = new Date(sunset * 1000);
 	const now = new Date();
 	return now > set;
-}
+};
 
-export const getSunPosition = (time) => {
-	const timeSinceMidnight = time==="now" ? new Date().getHours() : new Date(time * 1000).getHours();
+export const getRelativeTime = time => {
+	const timeInSeconds = new Date(time * 1000);
+	return moment(timeInSeconds).fromNow();
+};
+
+export const getSunPosition = time => {
+	const timeSinceMidnight = time === 'now' ? new Date().getHours() : new Date(time * 1000).getHours();
 	const dayProportion = timeSinceMidnight / 24;
-	return (dayProportion * 240) - 10;
-}
+	return dayProportion * 240 - 10;
+};
